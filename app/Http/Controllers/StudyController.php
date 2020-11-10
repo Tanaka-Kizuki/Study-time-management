109ac7d575057297b9a0102db24891d5ac6ce5f4
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-11-10 23:14:52 +0900

    create migration

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class StudyController extends Controller
 {
-    //
+    public function index() {
+        return view('index');
+    }
+
+    
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,16 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
+     <link rel="stylesheet" href="{{ asset('/css/index.css')}}">
 </head>
 <body>
      <div class="container">
-          <img class="logo" src="{{ asset('image/logo.svg') }}" alt="logo">
+          <div class="logo">
+               <img class="logo" src="{{ asset('/img/logo.svg') }}" alt="logo">
+          </div>
+          <div class="content">
+               <a href="/start">Let's Study</a>
+          </div>          
      </div>
 </body>
 </html>
