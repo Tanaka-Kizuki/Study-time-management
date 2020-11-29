6e300d238ef166b0ae69a0cff62b71839d4f7084
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-11-29 21:38:37 +0900

    update finish front

--- resources/views/finish.blade.php
+++ resources/views/finish.blade.php
@@ -3,13 +3,17 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="{{ asset('/css/finish.css')}}">
      <title>Document</title>
 </head>
 <body>
-     <form action="/finish">
-     @csrf
-          <textarea name="memo"></textarea>
-          <button>Finish</button>
-     </form>
+     <div class="container">
+          <h3>Memo</h3>
+          <form action="/finish">
+          @csrf
+               <textarea name="memo"></textarea>
+               <button>Finish</button>
+          </form>
+     </div>
 </body>
 </html>
