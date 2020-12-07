3d13b9c34c90349cedd0f82fcde49b563831508b
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-12-08 01:52:21 +0900

    勉強記録テスト　成功

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -31,5 +31,31 @@ class ExampleTest extends DuskTestCase
                 ->clickLink('Register')
                 ->assertPathIs('/register');
         });
+
+        //勉強記録がされてるかテスト
+        $this->browse(function ($first, $second) {
+            $user = factory(User::class)->create([
+                'email' => 'test@laravel.com',
+                'password' => 'secret'
+            ]);
+
+            $first->loginAs(User::find(1))
+                  ->visit('/home')
+                  ->assertSee('書籍登録')
+                  ->clickLink('START')
+                  ->assertPathIs('/start/record')
+                  ->check('book_id')
+                  ->type('subject','Laravel')
+                  ->press("Let's Study")
+                  ->visit('/home')
+                  ->assertSee('Studying Now')
+                  ->clickLink('FINISH')
+                  ->assertPathIs('/finish/record')
+                  ->type('memo','Laravel Finish')
+                  ->press("Finish")
+                  ->visit('/home')
+                  ->assertSee('Laravel')
+                  ->assertSee('Laravel Finish');
+        });
     }
 }
