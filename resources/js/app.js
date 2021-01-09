0f00c7f94a055d5ffa269569a82ba9e741695a24
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-01-09 22:48:00 +0900

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('booksearch-component', require('./components/BookSearchComponent.vue').default);
+Vue.component('like-component', require('./components/LikeComponent.vue').default);
 
 
 /**
