3704ee783263f4d8bdaf3332d7c93a42062b3e9d
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-01-09 22:50:28 +0900

    create LikeController

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Like;
+use App\Study;
+use Auth;
+
+class LikeController extends Controller
+{
+    public function firstcheck($post) {
+        $user = Auth::user();
+        $likes = new Like();
+        $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
+        if($like) {
+             return $like->like;
+        } else {
+             $like = $likes->create([
+                  'user_id' => $user->id,
+                  'posts_id' => $post,
+                  'like' => 0
+             ]);
+   
+             return $like->like;
+        }
+       }
+   
+       public function check($post) {
+        $user = Auth::user();
+        $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
+        if($like->like == 1) {
+             $like->like = 0;
+             $like->save();
+             return $like->like;
+        } else {
+             $like->like = 1;
+             $like->save();
+             return $like->like;
+        };
+       }
+}
