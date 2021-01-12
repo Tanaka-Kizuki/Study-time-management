1faeeb9c784e5c3db2af05509ab779878534d909
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-01-12 23:00:56 +0900

    update LikeController

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -7,36 +7,39 @@ use App\Like;
 use App\Study;
 use Auth;
 
-class LikeController extends Controller
-{
-    public function firstcheck($post) {
-        $user = Auth::user();
-        $likes = new Like();
-        $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
-        if($like) {
-             return $like->like;
-        } else {
-             $like = $likes->create([
-                  'user_id' => $user->id,
-                  'posts_id' => $post,
-                  'like' => 0
-             ]);
-   
-             return $like->like;
-        }
-       }
-   
-       public function check($post) {
-        $user = Auth::user();
-        $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
-        if($like->like == 1) {
-             $like->like = 0;
-             $like->save();
-             return $like->like;
-        } else {
-             $like->like = 1;
-             $like->save();
-             return $like->like;
-        };
-       }
+class LikeController extends Controller {
+     public function firstcheck($post) {
+          $user = Auth::user();
+          $likes = new Like();
+          $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
+          if($like) {
+               $count = $likes->where('posts_id',$post)->where('like',1)->count();
+               return [$like->like,$count];
+          } else {
+               $like = $likes->create([
+                    'user_id' => $user->id,
+                    'posts_id' => $post,
+                    'like' => 0
+               ]);
+               $count = $likes->where('posts_id',$post)->where('like',1)->count();
+               return [$like->like,$count];
+          }
+     }
+     
+     public function check($post) {
+          $user = Auth::user();
+          $likes = new Like();
+          $like = Like::where('posts_id',$post)->where('user_id',$user->id)->first();
+          if($like->like == 1) {
+               $like->like = 0;
+               $like->save();
+               $count = $likes->where('posts_id',$post)->where('like',1)->count();
+               return [$like->like,$count];
+          } else {
+               $like->like = 1;
+               $like->save();
+               $count = $likes->where('posts_id',$post)->where('like',1)->count();
+               return [$like->like,$count];
+          };
+     }
 }
