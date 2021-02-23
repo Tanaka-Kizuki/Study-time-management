ee9dfc308beaabe401264475a50b208aea373f69
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-02-23 21:23:44 +0900

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>StudyShear</title>
         <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
