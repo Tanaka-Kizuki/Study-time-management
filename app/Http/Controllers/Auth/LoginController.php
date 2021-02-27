a0781279997869eb381b645ef210192209099900
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-02-27 22:45:49 +0900

    update LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
-// use App\Http\Controllers\Auth;
+use App\Http\Controllers\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
