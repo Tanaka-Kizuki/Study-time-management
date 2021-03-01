d6e9ff7447129c9a44c57739a6663867ac111ecb
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-03-01 22:53:51 +0900

    format StudyController

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -2,71 +2,75 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Carbon\Carbon;
-use App\Study;
 use App\Book;
-use App\User;
 use App\Image;
+use App\Study;
+use App\User;
 use Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class StudyController extends Controller
 {
 
-    public function index() {
-        try{
+    public function index()
+    {
+        try {
             $user = User::all();
-        $book = Book::all();
-        $params = Study::orderBy('id','desc')->get();
-        $login = Auth::user();
-        $image = Image::where('user_id',$login->id)->first();
-        if($image == NULL) {
-            $image = new Image();
-            $image->user_id = $login->id;
-            $image->save();
-        }
-        if(Auth::check()) {
-            return view('index',['params' => $params,'user' => $user,'login' => $login,'book' => $book,'image'=>$image]);
-        }else {
-            return redirect('/');
-        };
+            $book = Book::all();
+            $params = Study::orderBy('id', 'desc')->get();
+            $login = Auth::user();
+            $image = Image::where('user_id', $login->id)->first();
+            if ($image == null) {
+                $image = new Image();
+                $image->user_id = $login->id;
+                $image->save();
+            }
+            if (Auth::check()) {
+                return view('index', ['params' => $params, 'user' => $user, 'login' => $login, 'book' => $book, 'image' => $image]);
+            } else {
+                return redirect('/');
+            };
         } catch (\Exception $ex) {
-       echo "<pre>";
-       var_dump($ex->getMessage());
-       echo "</pre>";
-       exit();
-       }
+            echo "<pre>";
+            var_dump($ex->getMessage());
+            echo "</pre>";
+            exit();
+        }
     }
 
-    public function howDo() {
+    public function howDo()
+    {
         return view('how');
     }
 
-    public function startRecord() {
+    public function startRecord()
+    {
         $user = Auth::user();
-        $books = Book::where('user_id',$user->id)->get();
-        return view('record',['books' => $books,'user' => $user]);
+        $books = Book::where('user_id', $user->id)->get();
+        return view('record', ['books' => $books, 'user' => $user]);
     }
 
-    public function start(Request $request) {
-        if($request->book_id) {
-            $subject = Book::where('id',$request->book_id)->first()->title;
+    public function start(Request $request)
+    {
+        if ($request->book_id) {
+            $subject = Book::where('id', $request->book_id)->first()->title;
         } else {
             $subject = $request->subject;
         }
-        $oldstudy = Study::orderBy('id','desc')->first();
+        $oldstudy = Study::orderBy('id', 'desc')->first();
         $start = Carbon::now();
-        $day = $start ->format('Y年m月d日');
-        if($oldstudy) {
-            if($oldstudy->finish) {
+        $day = $start->format('Y年m月d日');
+        if ($oldstudy) {
+            if ($oldstudy->finish) {
                 $study = Study::create([
                     'user_id' => Auth::id(),
                     'book_id' => $request->book_id,
                     'start' => $start,
-                    'time_start'=> $start->format('H時i分'),
+                    'time_start' => $start->format('H時i分'),
                     'status' => '勉強中',
                     'today' => $day,
-                    'subject' => $subject
+                    'subject' => $subject,
                 ]);
                 return redirect('/home');
             } else {
@@ -77,7 +81,7 @@ class StudyController extends Controller
                 'user_id' => Auth::id(),
                 'book_id' => $request->book_id,
                 'start' => $start,
-                'time_start'=> $start->format('H時i分'),
+                'time_start' => $start->format('H時i分'),
                 'status' => '勉強中',
                 'today' => $day,
                 'subject' => $subject,
@@ -86,27 +90,29 @@ class StudyController extends Controller
         }
     }
 
-    public function finishRecord() {
+    public function finishRecord()
+    {
         return view('finish');
     }
 
-    public function finish(Request $request) {
-        $oldstudy = Study::orderBy('id','desc')->first();
+    public function finish(Request $request)
+    {
+        $oldstudy = Study::orderBy('id', 'desc')->first();
         $start = new Carbon($oldstudy->start);
         $finish = Carbon::now();
         $todalTime = $start->diffInMinutes($finish);
         $totalTimeHours = ceil($todalTime / 15) * 0.25;
 
-        if($oldstudy) {
-            if($oldstudy->finish) {
+        if ($oldstudy) {
+            if ($oldstudy->finish) {
                 return redirect()->back();
-            } elseif($oldstudy->start) {
+            } elseif ($oldstudy->start) {
                 $oldstudy->update([
                     'finish' => $finish,
-                    'time_finish'=> $finish->format('H時i分'),
+                    'time_finish' => $finish->format('H時i分'),
                     'totaltime' => $totalTimeHours,
                     'memo' => $request->memo,
-                    'status' => '勉強終了!!!'
+                    'status' => '勉強終了!!!',
                 ]);
                 return redirect('/home');
             }
@@ -115,18 +121,20 @@ class StudyController extends Controller
         }
     }
 
-    public function book(Request $request) {
-        $books = Book::where('user_id',$request->id)->get();
+    public function book(Request $request)
+    {
+        $books = Book::where('user_id', $request->id)->get();
         $login = Auth::user();
-        return view('book',['books'=>$books,]);
+        return view('book', ['books' => $books]);
     }
 
-    public function bookadd(Request $request) {
+    public function bookadd(Request $request)
+    {
         $user = Auth::user();
         $book = Book::create([
             'user_id' => $user->id,
             'title' => $request->title,
-            'image' => $request->cover
+            'image' => $request->cover,
         ]);
         return redirect()->back();
     }
